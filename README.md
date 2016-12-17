a8031397c4d462028d9f51905d05b15ef7486fa8
tomaskraus <tomas.krausik@gmail.com>
2016-12-17 01:06:57 +0100

    refactor: rename fields. update help. update readme

--- README.md
+++ README.md
@@ -3,12 +3,22 @@ repo-extract
 
 ```
 <<< repo-extract - Git project isolation tool >>>
-Isolates a subdirectory from a repository and creates a new repository with a subdirectory content, with its history preserved.
+Isolates a subdirectory from a repository cloned from a remote and creates a new repository with that subdirectory content, with its history preserved.
+That new repository is then pushed to the destination remote.
 
-  usage: repo-extract.sh <original-git-remote> <dir-to-isolate> <new-repo-name> [<new-remote-origin-location>]
+  usage: repo-extract.sh <source-remote> <dir-to-isolate> <new-repo-name> [<destination-remote>]
 
-   if <new-remote-origin-location> is not specified, uses a temporary origin - this is a safe, non-destructive option for testing
+    if <destination-remote> is not specified, uses a local, temporary remote as a destination. This is a safe, non-destructive option for testing purposes.
 
-  note: provide an absolute path (or URL) for <original-git-remote> and <new-remote-origin-location> parameters
+  At the end, clones a temporary repository from the updated destination-remote, to see the fresh result.
 
+  note: provide an absolute path (or URL) for <source-remote> and <destination-remote> parameters
+  note: if <destination-remote> is specified, that destination-remote must already exists
+
+
+  example: repo-extract.sh "https://github.com/user/all-project" project1 project-1 "https://github.com/user/project-1"
 ```
+
+### tip
+
+When extracting multiple directories from one source-remote repository, speed-up the cloning process by clone that remote repository to your machine first, and then use that cloned one as a local source-remote.
--- repo-extract.sh
+++ repo-extract.sh
@@ -13,39 +13,45 @@ if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]; then
     #also update README.md
     echo " "
     echo "<<< repo-extract - Git project isolation tool >>>"
-    echo "Isolates a subdirectory from a repository and creates a new repository with a subdirectory content, with its history preserved."
+    echo "Isolates a subdirectory from a repository cloned from a remote and creates a new repository with that subdirectory content, with its history preserved."
+    echo "That new repository is then pushed to the destination remote."
     echo " "
-    echo "  usage: repo-extract.sh <original-git-remote> <dir-to-isolate> <new-repo-name> [<new-remote-origin-location>]"
+    echo "  usage: repo-extract.sh <source-remote> <dir-to-isolate> <new-repo-name> [<destination-remote>]"
     echo " "
-    echo "   if <new-remote-origin-location> is not specified, uses a temporary origin - this is a safe, non-destructive option for testing"
+    echo "    if <destination-remote> is not specified, uses a local, temporary remote as a destination. This is a safe, non-destructive option for testing purposes."
     echo " "
-    echo "  note: provide an absolute path (or URL) for <original-git-remote> and <new-remote-origin-location> parameters"
+    echo "  At the end, clones a temporary repository from the updated destination-remote, to see the fresh result."
+    echo " "
+    echo "  note: provide an absolute path (or URL) for <source-remote> and <destination-remote> parameters"
+    echo "  note: if <destination-remote> is specified, that destination-remote must already exists"
+    echo " "
+    echo " "
+    echo "  example: repo-extract.sh \"https://github.com/user/all-project\" project1 project-1 \"https://github.com/user/project-1\""
     echo " "
 else
 
     INITIAL_DIR=$PWD
 
-    ORIG_GIT_REMOTE=$1
+    SOURCE_REMOTE=$1
     DIR_TO_ISOLATE=$2
     NEW_REPO_NAME=$3
 
 
-    echo "-- initialize temporary dir: $INITIAL_DIR/temp"
-        TMP_DIR_NAME=$INITIAL_DIR/temp
-        TMP_DIR_REPO=$TMP_DIR_NAME/orig-repo
-        rm -r -f $TMP_DIR_NAME
+    TMP_DIR_NAME=$INITIAL_DIR/temp
+    echo "-- initialize temporary dir: $TMP_DIR_NAME"
+    rm -r -f $TMP_DIR_NAME
     #avoid continuation if an error occurs during the deletion
     if [ -d $TMP_DIR_NAME ]; then
         return -1
     fi
     mkdir $TMP_DIR_NAME
 
+    TMP_DIR_REPO=$TMP_DIR_NAME/source-repo-copy
+    echo "-- clone source remote: $SOURCE_REMOTE to $TMP_DIR_REPO"
+    git clone $SOURCE_REMOTE $TMP_DIR_REPO
 
-    echo "-- clone original remote: $ORIG_GIT_REMOTE to temp"
-    git clone $ORIG_GIT_REMOTE $TMP_DIR_REPO
 
-
-    echo "-- isolate git dir: $DIR_TO_ISOLATE"
+    echo "-- isolate git dir: $DIR_TO_ISOLATE from $TMP_DIR_REPO"
     cd $TMP_DIR_REPO
     #re-create the working copy
     git reset HEAD --hard
@@ -55,26 +61,25 @@ else
 
 
     if [ -z "$4" ]; then
-        NEW_REMOTE_ORIGIN=$TMP_DIR_NAME/origin
-        git init --bare $NEW_REMOTE_ORIGIN
-        echo "---- No remote origin specified. Using a temporary remote origin: $NEW_REMOTE_ORIGIN"
+        DESTINATION_REMOTE=$TMP_DIR_NAME/destination-remote
+        echo "---- No destination remote specified. Using a temporary destination remote: $DESTINATION_REMOTE"
+        git init --bare $DESTINATION_REMOTE
     else
-        NEW_REMOTE_ORIGIN=$4
+        DESTINATION_REMOTE=$4
     fi
 
-    echo "-- set to a new remote: $NEW_REMOTE_ORIGIN"
+    echo "-- set a new remote: $DESTINATION_REMOTE on $TMP_DIR_REPO"
     cd $TMP_DIR_REPO
     git remote remove origin
-    git remote add origin $NEW_REMOTE_ORIGIN
+    git remote add origin $DESTINATION_REMOTE
     git push origin --all
 
 
     cd $INITIAL_DIR
 
-    echo "-- test clone from: $NEW_REMOTE_ORIGIN"
+    echo "-- clone from: $DESTINATION_REMOTE"
     NEW_REPO_CLONED=$TMP_DIR_NAME/$NEW_REPO_NAME
-    git clone $NEW_REMOTE_ORIGIN $NEW_REPO_CLONED
-
-    echo "-- New cloned repo is at: $NEW_REPO_CLONED"
+    git clone $DESTINATION_REMOTE $NEW_REPO_CLONED
+    echo "-- newly cloned repo for preview is at: $NEW_REPO_CLONED"
 
 fi
